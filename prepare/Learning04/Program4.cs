f93b6658cac5c9b32e406dd7680139eb131a0556
Bryan Singer <brysinger99@gmail.com>
2023-11-08 10:01:49 -0700

    Started Learning 5

---
+++ prepare/Learning04/Program4.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program4 
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Learning04 World!");
+    }
+}
+
+public class Assignment
+{
+    private string _studntName;
+    private string _topic;
+    
+}
